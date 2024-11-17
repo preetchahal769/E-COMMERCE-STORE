3a22b8d86598b4251ebebe59ed70b5e8aaa101b9
Cyberrayon04846 <cyberrayon@gmail.com>
2024-11-17 09:57:32 +0530

    SignUp Page
    SignUp page Ui completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import { ShoppingCart, UserPlus, LogIn, LogOut, Lock } from "lucide-react";
 import { Link } from "react-router-dom";
 const Navbar = () => {
-  const user = true;
-  const isAdmin = true;
+  const user = false;
+  const isAdmin = false;
   return (
     <header className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800">
       <div className="container mx-auto px-4 py-3">
@@ -31,7 +31,7 @@ const Navbar = () => {
                   size={20}
                 />
                 <span className="hidden sm:inline ">Cart</span>
-                <span className="absolute -top-2  -right-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out">
+                <span className="absolute -top-2  -left-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out">
                   3
                 </span>
               </Link>
@@ -46,7 +46,7 @@ const Navbar = () => {
               </Link>
             )}
             {user ? (
-              <button className="bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 rounded-md flex item-center transition duration-300 ease-in-out">
+              <button className="bg-gray-700 hover:bg-gray-600 text-white py-1 px-4 rounded-md flex item-center transition duration-300 ease-in-out">
                 <LogOut size={18} className="mt-1" />
                 <span className="hidden sm:inline ml-2  ">Logout</span>
               </button>
