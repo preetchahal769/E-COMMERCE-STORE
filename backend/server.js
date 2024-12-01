9efeb38436f1708600e4045e4781a712d38bc19f
Cyberrayon04846 <cyberrayon@gmail.com>
2024-12-01 21:08:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 // Routes Imports
 import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/products.routes.js";
@@ -17,7 +17,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const __dirname = path.resolve();
 // auth routes
 
 app.use(express.json({ limit: "10mb" }));
@@ -28,6 +28,15 @@ app.use("/api/coupons", couponsRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 app.use("/api/cart", cartRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:5000`);
   connectToDB();
